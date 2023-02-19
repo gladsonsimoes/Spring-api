b04d793ebc841c5b6ccabed44a252a90804fb0af
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-02-18 21:17:00 -0300

    coloquei o objeto cliente2 de forma errada e consertei esse erro

--- src/main/java/com/project/javaspring/api/controller/ClienteController.java
+++ src/main/java/com/project/javaspring/api/controller/ClienteController.java
@@ -20,10 +20,10 @@ public class ClienteController {
         cliente1.setTelefone("5559888777");
 
         var cliente2 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Joao");
-        cliente1.setEmail("joao@email.com");
-        cliente1.setTelefone("5559866667");
+        cliente2.setId(2L);
+        cliente2.setNome("Joao");
+        cliente2.setEmail("joao@email.com");
+        cliente2.setTelefone("5559866667");
 
         return Arrays.asList(cliente1 , cliente2);
     }

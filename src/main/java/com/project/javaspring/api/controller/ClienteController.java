45f6527d26b7b30a313ec4bd096236c0162b8c8e
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-02-18 21:11:37 -0300

    Setar dados ao o objeto Cliente

--- src/main/java/com/project/javaspring/api/controller/ClienteController.java
+++ src/main/java/com/project/javaspring/api/controller/ClienteController.java
@@ -1,13 +1,30 @@
 package com.project.javaspring.api.controller;
 
+import com.project.javaspring.domain.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class ClienteController {
 
     @GetMapping("/clientes")
-    public String listar(){
-        return "Teste";
+    public List<Cliente> listar(){
+        var cliente1 = new Cliente();
+        cliente1.setId(1L);
+        cliente1.setNome("Gladson");
+        cliente1.setEmail("gladsonssimoes@email.com");
+        cliente1.setTelefone("5559888777");
+
+        var cliente2 = new Cliente();
+        cliente1.setId(1L);
+        cliente1.setNome("Joao");
+        cliente1.setEmail("joao@email.com");
+        cliente1.setTelefone("5559866667");
+
+        return Arrays.asList(cliente1 , cliente2);
     }
 }

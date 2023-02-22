64dfea0db2ea66a0f9b308c504a386d2aaf30770
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-02-22 12:21:32 -0300

    Obter somente o dados especificado pelo id

--- src/main/java/com/project/javaspring/api/controller/ClienteController.java
+++ src/main/java/com/project/javaspring/api/controller/ClienteController.java
@@ -5,21 +5,33 @@ import com.project.javaspring.domain.repository.ClienteRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/clientes") //em vez de usar a requisição /clientes em cada requisição
 public class ClienteController {
 
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @GetMapping("/clientes")
-    public List<Cliente> listar(){
+    @GetMapping
+    public List<Cliente> listar() {
         return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{clienteId}") //definir a busca do por id
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+        return clienteRepository.findById(clienteId)
+                .map(ResponseEntity::ok) //se caso existir 200 OK
+                .orElse(ResponseEntity.notFound().build()); //senão 404
     }
 }

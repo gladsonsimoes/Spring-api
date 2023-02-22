2370ccc6ba2b8a04e3b69185fcff60ce194f331d
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-02-22 09:54:48 -0300

    Pegar Dados Através de um Repository

--- src/main/java/com/project/javaspring/api/controller/ClienteController.java
+++ src/main/java/com/project/javaspring/api/controller/ClienteController.java
@@ -1,8 +1,10 @@
 package com.project.javaspring.api.controller;
 
 import com.project.javaspring.domain.model.Cliente;
+import com.project.javaspring.domain.repository.ClienteRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +15,11 @@ import java.util.List;
 @RestController
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    @Autowired
+    private ClienteRepository clienteRepository;
+
     @GetMapping("/clientes")
     public List<Cliente> listar(){
-    //Retornando Consulta da Tabela CLiente do Banco de dados com a classe resultante Cliente
-        return manager.createQuery("from Cliente", Cliente.class)
-        .getResultList();
+        return clienteRepository.findAll();
     }
 }

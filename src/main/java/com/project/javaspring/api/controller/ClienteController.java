24db7128bc879a777068809380f31bf0cc59cf1f
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-02-23 15:53:50 -0300

    alterei alguns códigos direto do repositório e coloque do catalogo

--- src/main/java/com/project/javaspring/api/controller/ClienteController.java
+++ src/main/java/com/project/javaspring/api/controller/ClienteController.java
@@ -2,7 +2,9 @@ package com.project.javaspring.api.controller;
 
 import com.project.javaspring.domain.model.Cliente;
 import com.project.javaspring.domain.repository.ClienteRepository;
+import com.project.javaspring.domain.service.CatalogoClienteService;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +12,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@AllArgsConstructor
 @RestController
 @RequestMapping("/clientes") //em vez de usar a requisição /clientes em cada requisição
 public class ClienteController {
 
-    @Autowired
     private ClienteRepository clienteRepository;
+    private CatalogoClienteService catalogoClienteService;
 
     @GetMapping
     public List<Cliente> listar() {
@@ -33,7 +35,7 @@ public class ClienteController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED) //definir como resposta 201
     public  Cliente adicionar(@Valid @RequestBody Cliente cliente){ //vinculando o corpo da requisição com o argumento cliente
-        return clienteRepository.save(cliente);
+        return catalogoClienteService.salvar(cliente);
     }
 
     @PutMapping("/{clienteId}")
@@ -42,7 +44,7 @@ public class ClienteController {
             return ResponseEntity.notFound().build();
         }
         cliente.setId(clienteId); //para identificar que tem um id para atualizar em vez criar um novo
-        cliente = clienteRepository.save(cliente);
+        cliente = catalogoClienteService.salvar(cliente);
 
         return ResponseEntity.ok(cliente);
     }
@@ -53,7 +55,7 @@ public class ClienteController {
             return ResponseEntity.notFound().build();
         }
 
-        clienteRepository.deleteById(clienteId);
+        catalogoClienteService.excluir(clienteId);
 
         return ResponseEntity.noContent().build();
     }
